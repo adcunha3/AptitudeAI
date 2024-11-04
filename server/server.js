e305a1de14deffd0a7ea45de93c42d1858766711
Adam DCunha <adamdcunha@Adams-MacBook-Air-2.local>
2024-11-03 23:27:25 -0500

    sql db connection and frontend cleanup

--- server/server.js
+++ server/server.js
@@ -1,4 +1,12 @@
 import express from 'express';
+import connection from './config/database.js';
+
 const app = express();
 
-app.listen(3001, () => {console.log("Server running on port 3001.")});
+app.listen(3000, () => {
+    console.log('App running on port 3000');
+    connection.connect((err) => {
+        if (err) throw err;
+        console.log('DB Connected');
+    })
+});
