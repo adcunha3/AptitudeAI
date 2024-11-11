d561d2204b1a3204bc25c3562f8d33e016f988ac
Adam DCunha <adamdcunha@Adams-MacBook-Air-2.local>
2024-11-11 15:15:14 -0500

    define user and roles collections

--- server/server.js
+++ server/server.js
@@ -2,7 +2,9 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import connectDB from './config/database.js';
+import connectDB from './config/db.config.js';
+import cookieSession from 'cookie-session';
+
 
 dotenv.config();
 
@@ -16,5 +18,17 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());     
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession(
+    {
+      name: "bezkoder-session",
+      keys: ["COOKIE_SECRET"],
+      httpOnly: true
+    }
+)); 
+
+// Test route
+app.get("/", (req, res) => {
+res.json({ message: "Welcome to bezkoder application." });
+});
 
 app.listen(3000, () => { console.log('App running on port 3000'); });
