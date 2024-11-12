35fe9f847acd4af73be8f20111f627696396d4bb
Adam DCunha <adamdcunha@Adams-MacBook-Air-2.local>
2024-11-12 15:06:49 -0500

    sign up middleware fix

--- server/middlewares/verifySignUp.js
+++ server/middlewares/verifySignUp.js
@@ -1,18 +1,21 @@
 const User = require("../models/user.model.js");
 
-const checkDuplicateUsernameOrEmail = (req, res, next) => {
-    const username = User.findOne({ username: req.body.username });
-    if (username) {
-        res.status(400).send({ message: "Error: Username is already in use." });
-        return;
-    }
+const checkDuplicateUsernameOrEmail = async (req, res, next) => {
+    try {
+        const username = await User.findOne({ username: req.body.username });
+        if (username) {
+            return res.status(400).send({ message: "Error: Username is already in use." });
+        }
 
-    const email = User.findOne({ email: req.body.email });
-    if (email) {
-        res.status(400).send({ message: "Error: Email is already in use." });
-        return;
+        const email = await User.findOne({ email: req.body.email });
+        if (email) {
+            return res.status(400).send({ message: "Error: Email is already in use." });
+        }
+
+        next();
+    } catch (error) {
+        return res.status(500).send({ message: error.message || "Some error occurred while checking for duplicates." });
     }
-    next();
 };
 
 const verifySignUp = {
