3504d76c3d67167cfc022f4a41f89fc6a1653e3e
Adam DCunha <adamdcunha@dyn172-30-117-111.wireless.uwo.pri>
2024-11-21 18:33:44 -0500

    endpoints to use id instead of username and verify token check

--- server/controllers/profile.controller.js
+++ server/controllers/profile.controller.js
@@ -1,9 +1,9 @@
 const User = require('../models/user.model.js');
-
+const mongoose = require('mongoose');
 
 exports.getProfile = async (req, res) => {
     try {
-        const user = await User.findOne({ username: req.params.username }).select('-password'); // Exclude password field
+        const user = await User.findOne({ _id: new mongoose.Types.ObjectId(req.params.id) }).select('-password');
         if (!user) {
             return res.status(404).send({ message: "User not found" });
         }
@@ -17,14 +17,16 @@ exports.getProfile = async (req, res) => {
 exports.updateProfile = async (req, res) => {
     try {
         const updatedData = {
-            username: req.body.usernamename,
-            email: req.body.email,
-            role: req.body.role,
             bio: req.body.bio,
-            interests: req.body.interests,
+            interests: req.body.interests
         };
 
-        const user = await User.findOneAndUpdate({ username: req.params.username }, updatedData, { new: true }).select('-password');
+        const user = await User.findOneAndUpdate(
+            { _id: new mongoose.Types.ObjectId(req.params.id) }, 
+            updatedData, 
+            { new: true }
+        ).select('-password');
+
         if (!user) {
             return res.status(404).send({ message: "User not found" });
         }
--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,6 +1,7 @@
-const verifySignUp = require("../middlewares/verifySignUp.js");
 const controller = require("../controllers/auth.controller.js");
 const profileController = require("../controllers/profile.controller.js");
+const verifyToken = require("../middlewares/verifyToken.js");
+const verifySignUp = require("../middlewares/verifySignUp.js");
 
 module.exports = function(app) {
     app.use(function(req, res, next) {
@@ -12,11 +13,9 @@ module.exports = function(app) {
     });
 
     app.post("/api/auth/signup", [verifySignUp.checkDuplicateUsernameOrEmail], controller.signup);
-
     app.post("/api/auth/signin", controller.signin);
-
     app.post("/api/auth/signout", controller.signout);
 
-    app.get("/api/profile/:username", profileController.getProfile); 
-    app.put("/api/profile/:username", profileController.updateProfile); 
+    app.get("/api/profile/:id", [verifyToken.checkToken], profileController.getProfile); 
+    app.put("/api/profile/:id", [verifyToken.checkToken], profileController.updateProfile); 
 }
